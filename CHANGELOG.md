285b1425b73bbfac7f42d712b307e0224d071fb8
Jure Mihelič <jure.mihelic@gmail.com>
2022-05-15 22:33:58 +0200

    Update: dart doc, example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,15 @@
+## 0.3.0
+
+* Add: dart doc comments
+* Update: example
+
+## 0.2.0
+
+* Tested (Android) and documented
+
 ## 0.1.0
 
 * Initial release
 * Support for Android phones (old Camera API as well as new camera2 API)
 * Support for iOS phones
-* Conformance to null safety.
+* Conformance to null safety
--- lib/torch_control.dart
+++ lib/torch_control.dart
@@ -5,27 +5,35 @@ import 'package:flutter/services.dart';
 class TorchControl {
   static const MethodChannel _channel = MethodChannel('torch_control');
 
+  /// Is the flashlight available?
   static Future<bool> ready() async {
     return await _channel.invokeMethod('ready');
   }
 
   static bool _isOn = false;
 
+  /// Is the flashlight on?
   static bool get isOn => _isOn;
 
+  /// Is the flashlight off?
   static bool get isOff => !_isOn;
 
+  /// Turn the flashlight on or off.
   static Future<bool> turn(bool state) async {
     _isOn = await _channel.invokeMethod('turn', {'state': state});
     return _isOn;
   }
 
+  /// Turn the flashlight on.
   static Future<bool> turnOn() async => turn(true);
 
+  /// Turn the flashlight off.
   static Future<bool> turnOff() async => turn(false);
 
+  /// Toggle the flashlight (on goes to off and vice versa).
   static Future<bool> toggle() async => turn(!isOn);
 
+  /// Flash the flashlight for the specified duration.
   static Future<void> flash(Duration duration) =>
       turnOn().whenComplete(() => Future.delayed(duration, turnOff));
 }
